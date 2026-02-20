a293017954e21f319d0ac2d2b276f283794f37af
erickfelip <erickfelipe2010@gmail.com>
2026-02-19 22:04:30 -0300

    force push

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -7,25 +7,25 @@ export const AuthProvider = ({ children }: any) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  // Verifica autenticação ao carregar
-//   useEffect(() => {
-//     checkAuthentication()
-//   }, []);
+  // Verifica autenticação ao carrega
+  //   useEffect(() => {
+  //     checkAuthentication()
+  //   }, []);
 
-//   const checkAuthentication = async () => {
-//     try {
-//       const userData = await authService.checkAuth();
-//       setUser(userData);
-//     } catch (error) {
-//       setUser(null);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
+  //   const checkAuthentication = async () => {
+  //     try {
+  //       const userData = await authService.checkAuth();
+  //       setUser(userData);
+  //     } catch (error) {
+  //       setUser(null);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
   const login = async (email: string, password: string) => {
     const data = await authService.login(email, password);
-    // APOS FAZER LOGIN DEVE SER BUSCADO EM UM ENDPOINT OS DADOS DO USUARIO 
+    // APOS FAZER LOGIN DEVE SER BUSCADO EM UM ENDPOINT OS DADOS DO USUARIO
     // E STAR NO ESTADO "USER"
     setUser(data);
     return data;
